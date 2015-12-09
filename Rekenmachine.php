1086babfa989ca49386317fc56cd01733325848d
unknown <147263@WKS-HP-1749.ASG.LOCAL>
2015-12-09 13:41:47 +0100

    rekenmachine toegevoegd maar dit kan weer weg

---
+++ Rekenmachine.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<head>
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
+	<title>Netbeans</title>
+	<link href="opmaak.css" rel="stylesheet"/>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+
+<div id=menu class=pos_fixed>
+<ul>
+<li><a href=http://goo.gl/XhsEBg>Hoofdpagina</a></li>
+<li><a href=hobbys.html>Hobby's</a></li>
+<li><a href=portfolio.html>Portfolio</a>
+	<ul class=subMenu>
+		<li><a href=favicon.html>Favicon</a></li>
+		<li><a href=tabel.html>Tabel</a></li>
+		<li><a href=contact.html>Formulier</a></li>
+		<li><a href=nfc.html>Hardware</a></li>
+		<li><a href=photoshop.html>Keuzeopdracht</a></li>
+		<li><a href=PHP.php>PHP</a></li>
+	</ul>
+</li>
+<li><a href=contact.html>Contact</a></li>	
+</ul>
+</div>
+
+<div id=container>
+<div id=titel>Netbeans</div>
+
+
+</body>
+
+</html>
